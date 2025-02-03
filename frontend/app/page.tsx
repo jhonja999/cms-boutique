0a3b384155c4cbf1414aa50f8a5242f4f89db543
Jhon Jairo Vargas <85378059+jhonja999@users.noreply.github.com>
2025-02-03 02:00:46 -0500

    frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+export default function Home() {
+  return (
+    <main>
+      hellofrom home
+      <Button>Button</Button>
+
+    </main>
+   
+  );
+}
---
+++ frontend/components/navbar.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { Heart, ShoppingCart, User } from "lucide-react";
+import { useRouter } from "next/navigation";
+import MenuList from "./menu-list";
+import ItemsMenuMobile from "./items-menu-mobile";
+
+const Navbar = () => {
+    const router = useRouter()
+
+
+    return (
+        <div className="flex items-center justify-between p-4 mx-auto cursor-pointer sm: max-w-4xl md: max-w-6xl">
+            <h1 className="text-3xl" onClick={() => router.push("/")}>CMS
+                <span className="font-bold">Boutique</span>
+            </h1>
+            <div className="items-center justify-between hidden sm:flex">
+                <MenuList/>
+            </div>
+            <div className="flex sm:hidden">
+                <ItemsMenuMobile/>
+            </div>
+
+            <div className="flex items-center justify-between gap-2 sm:gap-7">
+                <ShoppingCart strokeWidth="1" className="cursor-pointer" onClick={() => router.push("/cart")} />
+                <Heart strokeWidth="1" className="cursor-pointer" onClick={() => router.push("/loved-products")} />
+
+                <User strokeWidth="1" className="cursor-pointer" />
+            </div>
+
+        </div>
+    );
+}
+
+export default Navbar;
