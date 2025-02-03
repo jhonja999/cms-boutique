04b77143f840a6e63da188a2f80b09a2691b5bd4
Jhon Jairo Vargas <85378059+jhonja999@users.noreply.github.com>
2025-02-03 02:53:23 -0500

    carousel

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button"
+import CarouselTextBanner from "@/components/carousel-text-banner";
 
 export default function Home() {
   return (
     <main>
-      hellofrom home
-      <Button>Button</Button>
+      <CarouselTextBanner />
 
     </main>
    
