eb59923ebb331ac19d15bb67fad19e6c88040e8a
Jhon Jairo Vargas <85378059+jhonja999@users.noreply.github.com>
2025-02-03 02:16:31 -0500

    theme

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-
+import { ThemeProvider } from "@/components/theme-provider"
+ 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,9 +29,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
         <Navbar/>
         {children}
         <Footer/>
+        </ThemeProvider>
       </body>
     </html>
   );
